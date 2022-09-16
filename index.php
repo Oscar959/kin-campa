a78aa62b40b68bae507cfd8cd577f0d2cb800228
oscar mongende <oscarmongende959@gmail.com>
2022-09-16 22:09:49 +0200

    making a link in my index page in order to redirect to admin to the login the page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The index page</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+</head>
+<style>
+    *{
+        font-family:verdana;
+    }
+</style>
+<body class="m-5">
+    <div class="row">
+        <div class="offset-md-2 col-md-8 offset-md-2">
+            <div class="card  text-center">
+                <div class="card-header alert alert-default">
+                    <p class="h3">Bienvenu dans la page d'accueil</p>
+                </div>
+
+                <div class="card-body">
+                    <p class="h6">
+                        Connectez vous en cliquant sur le <a href="admins/login.php">lien ici </a> 
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
