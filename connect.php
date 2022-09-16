d7642a70794171c573ee950e97b75d0a4677ac98
oscar mongende <oscarmongende959@gmail.com>
2022-09-16 23:31:20 +0200

    the connect.php contains my connections to the db

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "campus");
+
+if($conn){
+    //echo "<p class='alert alert-danger'><button class='btn btn-outline-danger'>Connection  reussie</button></p>";
+}
